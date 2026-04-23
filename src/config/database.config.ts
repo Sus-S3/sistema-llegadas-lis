0274a925f5744e870ff8dcb7f4556338eec0c64d
Susana Suarez <susana.suareza@udea.edu.co>
2026-04-23 16:41:21 -0500

    fix: usar DATABASE_URL en produccion

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,6 +5,7 @@ export default registerAs(
   'database',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
+    url: process.env.DATABASE_URL,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT ?? '5432', 10),
     database: process.env.DB_NAME,
@@ -13,5 +14,6 @@ export default registerAs(
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: process.env.NODE_ENV !== 'production',
     logging: process.env.NODE_ENV === 'development',
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
   }),
 );
